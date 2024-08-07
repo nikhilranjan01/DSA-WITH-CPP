9a787510ea8eb0e93b59370851535594629bc801
nikhilranjan01 <nikhilranjan2004@gmail.com>
2024-08-08 01:29:36 +0530

    New Update

---
+++ Sizeprint.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char a = 'N';
+    cout<< a << endl;
+    float b = 1.2;
+    cout<< b << endl;
+    double c = 12.33;
+    cout<< c << endl;
+    int size = (c);
+    cout <<"size of a is:" << size << endl;
+    bool bl = false;
+    cout<< bl <<endl; 
+}
