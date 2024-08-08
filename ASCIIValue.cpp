e57fa58a76d5cd2a00a9d15df2d8d4ea97582738
nikhilranjan01 <nikhilranjan2004@gmail.com>
2024-08-09 01:47:25 +0530

    New Update

---
+++ ASCIIValue.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main (){
+    char a = 'N';
+    char b = 'I';
+    char c = 'K';
+    char d = 'H';
+    char e = 'I';
+    char f = 'L';
+    cout<< a+b+c+d+e+f << endl;
+    
+}
